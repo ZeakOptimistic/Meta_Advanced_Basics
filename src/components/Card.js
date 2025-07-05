997856c005ac29abd05cae776cc5e7121240579f
Zeak Optimistic <quangkhaint2005@gmail.com>
2025-07-05 16:53:08 +0700

    Finish

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,34 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+
+    <VStack 
+    color='black' 
+    backgroundColor='white'
+    borderRadius='xl'
+    cursor='pointer'
+    >
+      <Image src={imageSrc} borderRadius='xl' />
+
+      <VStack spacing={5} p={5} alignItems="flex-start">
+
+        <HStack justifyContent='space-between' alignItems='center'>
+          <Heading as='h3' size='md'>{title}</Heading>
+        </HStack>
+
+        <Text fontSize='lg' color='slategray'>{description}</Text>
+
+        <HStack>
+        <p>See more</p>
+        <FontAwesomeIcon icon={faArrowRight} size="1x" />
+        </HStack>
+
+      </VStack>
+
+    </VStack>
+    
+  );
 };
 
 export default Card;
--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -15,7 +15,21 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
+    <VStack spacing={10}>
 
+      <Avatar src="https://i.pravatar.cc/150?img=7" 
+      size='2xl' 
+      name="Your Name" 
+      />
+
+      <Heading as='h4' size='md' noOfLines={1}>{greeting}</Heading>
+
+      <VStack spacing={10}>
+       <Heading as='h1' size='3xl' noOfLines={1}>{bio1}</Heading>
+       <Heading as='h1' size='3xl' noOfLines={1}>{bio2}</Heading>
+      </VStack>
+
+    </VStack>
   </FullScreenSection>
 );
 
